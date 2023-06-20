8d9d4c9f33418cf9f9117dc6069c0679ff020c9b
bd-mamunur <mamunur@bdthemes.com>
2023-06-20 15:16:55 +0600

    color tab is styled

--- build/blocks/advanced-icon-box/index.asset.php
+++ build/blocks/advanced-icon-box/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0a4d9cc23eaa06fc2851');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '68df56c17516377b5602');
--- build/blocks/icon-box-item/index.asset.php
+++ build/blocks/icon-box-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8019688158f5b117e534');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '702f48d56f7f01ecf3ab');
